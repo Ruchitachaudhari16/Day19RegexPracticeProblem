dda889051849cb1377a2687eb4bc8da56f283447
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-11 15:03:09 -0400

    [Ruchita] Add: validate firstname using Regex

---
+++ RegXPracticeProblem/RegXPracticeProblem/Pattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegXPracticeProblem
+{
+   public class Pattern
+    {
+        //UC1:- As a User need to enter a valid FirstName- First name starts with Cap and has minimum 3 characters
+        public void ValidateFirstName()
+        {
+            Console.WriteLine("Enter the First name ");
+            string name = Console.ReadLine();
+            string firstName = "^[A-Z]{1}[a-z]{3,}?";
+            if (Regex.IsMatch(name, firstName))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name is INVALID");
+            }
+        }
+
+    }
+}
---
+++ RegXPracticeProblem/RegXPracticeProblem/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegXPracticeProblem
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine("Regex");
+            while (true) 
+            {
+
+                Console.WriteLine("Regex");
+                Console.WriteLine("Choose correct  options");
+                Console.WriteLine("1:First Name");
+                int choice = Convert.ToInt32(Console.ReadLine());
+               
+                switch (choice)
+                {
+                    case 1:
+                        pattern.ValidateFirstName();
+                        Console.WriteLine("--------------------------");
+                        break;
+
+                }
+            }
+        }
+    }
+}
