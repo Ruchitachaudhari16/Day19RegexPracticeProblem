a9405570def0cedc449df8fc8f3fff3cc8463146
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-07-13 23:50:52 -0400

    [Ruchita]Add: should have atleast one special character

--- RegXPracticeProblem/RegXPracticeProblem/Pattern.cs
+++ RegXPracticeProblem/RegXPracticeProblem/Pattern.cs
@@ -70,10 +70,10 @@ namespace RegXPracticeProblem
         }
         public void ValidatePassword()
         {
-            //Should have atleast one numeric number
+            //Should have atleast one special character
             Console.WriteLine("Enter the Password");
             string password = Console.ReadLine();
-            string pass = "^(?=.*?[A-Z])(?=.*?[0-9])[0-9A-Za-z@#$%&!]{8,}$";
+            string pass = "^(?=[a-zA-Z0-9#@$?-_]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$";
             if (Regex.IsMatch(password,pass))
             {
                 Console.WriteLine("Password is valid");
